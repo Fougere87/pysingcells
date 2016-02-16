15917f5a4ba34f4b7b06eb0da35d4481a3ec80b9
Pierre Marijon <pierre@marijon.fr>
2016-02-16 17:06:30 +0100

    Try to add log level in logging system

--- pysingcells/logger.py
+++ pysingcells/logger.py
@@ -10,12 +10,19 @@ import logging
 # fief import
 from fief import filter_effective_parameters as fief
 
+log = logging.getLogger()
+
 @fief
 def setup_logging(options):
     
-    log = logging.getLogger()
+    log.setLevel(10 if "logging_level" in options else
+                 options["logging_level"])
 
-    log.setLevel(10 if "logging_level" in options else options["logging_level"])
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter('%(levelname)s :: %(message)s'))
+    handler.setLevel(10 if "logging_level" in options else
+                     options["logging_level"])
+
     log.addHandler(logging.StreamHandler(sys.stderr))
 
-log = logging.getLogger()
+
