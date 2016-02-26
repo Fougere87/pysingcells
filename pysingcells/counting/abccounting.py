ae8a4a051130eb04e16b09e88e02e0590923b98b
Pierre Marijon <pierre@marijon.fr>
2016-02-26 13:14:40 +0100

    Add counting module with featurecounts appli

---
+++ pysingcells/counting/abccounting.py
@@ -0,0 +1,141 @@
+
+# -*- coding: utf-8 -*-
+
+# std import
+import os
+from abc import ABCMeta, abstractmethod
+
+# fief import
+from fief import filter_effective_parameters as fief
+
+# project import
+from ..logger import log
+from ..abcstep import AbcStep, StepStat
+
+# tested import
+try:
+    from os import scandir
+except ImportError:
+    from scandir import scandir
+
+class AbcCounting(AbcStep, metaclass=ABCMeta):
+    """ Abstract class for mapper """
+    
+    def __init__(self):
+        """ Initiliaze couting """
+        super().__init__()
+        self.name = ""
+        self.bin_path = ""
+        self.in_path = ""
+        self.out_path = ""
+        self.log_dir = ""
+        self.options = ""
+        self.annotation_path = ""
+
+        # command flag
+        self.annotation_flag = ""
+        self.input_flag = ""
+        self.output_flag = ""
+
+    @fief
+    def read_configuration(self, counting):
+        """ read configuration of counting """
+        
+        log.info(self.get_name() + " read configuration")
+
+        self.name = counting["name"]
+        self.bin_path = counting["bin_path"]
+        self.in_path = counting["in_path"]
+        self.out_path = counting["out_path"]
+        self.log_dir = counting["log"]
+        self.options = counting["options"]
+        self.annotation_path = counting["annotation_path"]
+
+        self.state = StepStat.load
+
+    def check_configuration(self):
+        """ Check if file, binary, other ressource is avaible for run count """
+
+        log.info(self.get_name() + " check configuration")
+
+        if self.state != StepStat.load:
+            log.critical("You are not in the good state to run this, maybe you \
+            have a problem.")
+            return False
+
+        if not self.name.lower() == self.get_name().lower() :
+            self.state = StepStat.no_ready
+
+            log.critical("Mapper name is differente of classname we can't use \
+            this class")
+            return False
+
+        if not os.path.isdir(self.in_path) :
+            self.state = StepStat.no_ready
+
+            log.critical("Path you set for in_path isn't a directory")
+            return False
+
+        if not os.path.isdir(self.out_path) :
+            self.state = StepStat.no_ready
+
+            log.critical("Path you set for out_path isn't a directory")
+            return False
+
+        if not os.path.isdir(self.log_dir) :
+            self.state = StepStat.no_ready
+
+            log.critical("Path you set for log_dir isn't a directory")
+            return False
+
+        if not os.path.isfile(self.annotation_path):
+            self.state = StepStat.no_ready
+
+            log.critical("Path you set for annotation_path isn't a file")
+            return False
+
+        self.state = StepStat.ready
+        return True
+            
+    def run(self):
+        """ Run the counting effectively """
+
+        log.info(self.get_name() + " run")
+
+        if self.state != StepStat.ready:
+            log.debug(" You are not in the good state to run this, maybe you \
+            have a problem.")
+            return False
+
+        if self._run_counting() and self._convert_output():
+            self.state = StepStat.succes
+        else:
+            self.state = StepStat.failled
+
+    def get_name(self):
+        """ Get the name of mapper """
+        return self.__class__.__name__
+
+    @abstractmethod
+    def _run_counting(self):
+        """ Run counting effectively """
+        pass
+
+    @abstractmethod
+    def _convert_output(self):
+        """ Convert output in useful format """
+        pass
+
+def tpm(count, length, nb_read):
+    """ Return the tpm value """
+    pm = nb_read / (10**6)
+    rpk = count / length
+
+    return rpk / pm
+
+
+def rpkm(count, length, nb_read):
+    """ Return the rpkm value """
+    pm = nb_read / (10**6)
+    rpm = count / pm
+    return rpm / length
---
+++ pysingcells/counting/featurecounts.py
@@ -0,0 +1,91 @@
+
+# -*- coding: utf-8 -*-
+
+# std import
+import subprocess
+
+# pandas import
+from pandas import DataFrame, read_csv
+
+# project import
+from ..logger import log
+from ..abcstep import StepStat
+from .abccounting import AbcCounting, tpm
+
+# tested import
+try:
+    from os import scandir
+except ImportError:
+    from scandir import scandir
+
+class FeatureCounts(AbcCounting):
+    """ Class for run Feature Counts """
+
+    def __init__(self):
+        """ Intialize featureCounts runner object """
+        super().__init__()
+        # command flag
+        self.annotation_flag = "-a"
+        self.input_flag = ""
+        self.output_flag = "-o"
+
+    def _run_counting(self):
+        """ Run count effectively """
+        
+        log.info(self.get_name() + " run raw count")
+
+        raw_output = self.out_path + "raw_counts.tsv"
+        list_file = [input_file.path for input_file in scandir(self.in_path) 
+                     if input_file.is_file() and
+                     input_file.name.endswith(".sam")]
+
+        base_command = [self.bin_path]
+        base_command += self.options.split(" ")
+        base_command += [self.annotation_flag, self.annotation_path,
+                         self.output_flag, raw_output, self.input_flag] 
+        base_command += list_file
+
+        process = subprocess.Popen(base_command, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+ 
+        self._write_process(base_command, self.get_name(), process)
+
+        return process.returncode == 0
+
+    def _convert_output(self):
+        """ Convert output in useful format """
+        
+        log.info(self.get_name() + " convert raw count in tpm ")
+
+        # reading
+        header = read_csv(self.out_path + "raw_counts.tsv", 
+                            header=1, sep='\t', chunksize=1)
+
+        col_type = {index: float if val == int else val 
+                    for index, val in header.read().dtypes.iteritems()}
+
+        raw_data = read_csv(self.out_path + "raw_counts.tsv", 
+                            header=1, sep='\t', dtype=col_type)
+        # cleaning
+        delete_col = ["Chr", "Start", "End", "Strand"]
+        for colname in delete_col:
+            raw_data.drop(colname, inplace=True, axis=1)
+
+        samples = raw_data.columns[2:]
+
+        for sample in samples:
+            log.info("\t for sample " + sample)
+            nb_heat = sum(raw_data[sample])
+
+            for index, count in raw_data[sample].iteritems():
+                raw_data.set_value(sample, index, tpm(count, 
+                                                      raw_data["Length"][index],
+                                                      nb_heat))
+
+        # more cleaning
+        raw_data.drop("Length", inplace=True, axis=1)
+
+        # writing
+        raw_data.to_csv(self.out_path + "clean_tpm.csv")
+
+        return True
